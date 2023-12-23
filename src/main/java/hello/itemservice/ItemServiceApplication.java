724b47126b75f0a8ce8ca58d6bdc576ff424d2cb
seonwoo_jung <laborlawseon@gmail.com>
2023-12-23 16:51:38 +0900

    섹션 3. 테스트 - 트랜잭션, 임베디드 모드 학습 완료

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,15 +2,20 @@ package hello.itemservice;
 
 import hello.itemservice.config.JdbcTemplateV3Config;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
+@Slf4j
 @Import(JdbcTemplateV3Config.class)
 // 컴포넌트 스캔 범위 지정(컨트롤러만), 지정하지 않으면 현재 패키지와 하위 패키지 모두 스캔함
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
@@ -24,5 +29,17 @@ public class ItemServiceApplication {
 	@Profile("local") // profile이 local로 설정되어 있을 때만 활성화 됨.
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	@Bean
+	@Profile("test")
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 }
